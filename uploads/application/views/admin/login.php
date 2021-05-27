edadf7ab0869609bd5961cae1947d8a929569055
Rexter Dayuta <rexterdayuta@gmail.com>
2021-05-27 22:18:08 +0800

    header and title rename

--- uploads/application/views/admin/login.php
+++ uploads/application/views/admin/login.php
@@ -1,4 +1,4 @@
-<title>COS ePoster Submission Site</title>
+<title>COS ePoster and Presentation Submission Site</title>
 <div class="content">
     <div class="container">
         <div class="row">
@@ -9,7 +9,7 @@
                 <div class="row justify-content-center">
                     <div class="col-md-8">
                         <div class="mb-4">
-                            <h4>COS ePoster Submission Site</h4>
+                            <h4>COS ePoster and Presentation Submission Site</h4>
                             <h5><strong>Admin Panel</strong></h5>
                         </div>
                         <form action="#" method="post">
